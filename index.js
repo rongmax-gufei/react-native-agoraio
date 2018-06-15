5014e1f286b099d028467c49e41f13875c042f7e
gufei <gufei@learnta.com>
2018-06-15 11:16:24 +0800

    Add Eslint
    代码更规范

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
+import _RtcEngine from './RtcEngine'
 import _AgoraVideoView from './AgoraVideoView'
 import _AgoraScreenShareView from './AgoraScreenShareView'
-import _RtcEngine from './RtcEngine'
 
+export const RtcEngine = _RtcEngine;
 export const AgoraVideoView = _AgoraVideoView;
 export const AgoraScreenShareView = _AgoraScreenShareView;
-export const RtcEngine = _RtcEngine;
